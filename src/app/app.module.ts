b2a2deeabbd12ba3c2073aee438571c4fd7bd7c7
jozk <jorgeriffo.u@gmail.com>
2023-09-13 12:54:13 -0300

    Se agrega la libreria para codigos QR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { NgChartsModule, NgChartsConfiguration } from 'ng2-charts'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
